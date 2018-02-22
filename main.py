d31a183ec6ce3eb0893223ec3bb3096912050bf2
islamusayman95 <islam_ayman95@yahoo.com>
2018-02-22 20:44:28 +0200

    Initial Setup

---
+++ main.py
@@ -0,0 +1,23 @@
+class Drone(object):
+    def __init__(self, key):
+        self.id = key
+        self.capacity = int()
+        self.product_items = None
+
+    def commander(self, command):
+        pass
+
+    def
+
+class ProductItem(object):
+    pass
+
+
+class ProductType(object):
+    pass
+
+
+class WareHouse(object):
+    pass
+
+class
